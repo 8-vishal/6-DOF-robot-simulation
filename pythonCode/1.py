7f4afc0bd13fe49f8532532378aa9887f498ebdf
Vishal Kumar <vishal.mi19@iiitmk.ac.in>
2020-09-30 16:40:54 +0530

    Create 1.py

---
+++ pythonCode/1.py
@@ -0,0 +1,15 @@
+import pybullet as sim
+import time
+import math
+
+
+pc = sim.connect(sim.GUI)
+sim.setGravity(0, 0, -9.8)
+
+path = "path to your URDF file"
+
+armID = sim.loadURDF(path, basePosition=[0, 0, 0], useFixedBase=True)
+
+while pc != 0:
+  sim.stepSimulation()
+  time.sleep(0.01)
